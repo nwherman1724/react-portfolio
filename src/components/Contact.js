f51e05a7bf81a9a036339e344973a201f6862f34
Theophilus <93491747+nwherman1724@users.noreply.github.com>
2023-04-23 20:43:00 -0400

    form edits

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,23 +1,62 @@
-import React from "react";
+import React, {useState} from "react";
 import "./Contact.css";
 
 function Contact() {
+
+    const [userName, setUserName] = useState('');
+    const [userEmail, setUserEmail] = useState('');
+    const [userMessage, setUserMessage] = useState('');
+
+    const handleInputChange = (e) => {
+        const { name, value } = e.target;
+    
+        return name = setUserName(value)
+      };
+
+    const handleFormSubmit = (e) => {
+        e.preventDefault();
+
+        
+    };
+
     return (
         <div class="contactCard" id="Contact">
             <form action="https://formsubmit.co/090af7c3aa391fc3128974ef4d08d01e" method="POST" >
                 <h3 class="contactTitle">Contact Me</h3>
 
-                {/* <label for="name">Name:</label>  */}
-                <input type="text" name="name" placeholder="Your Name" required/> <br/>
+                <input 
+                    value=""
+                    type="text"
+                    onChange={handleInputChange}
+                    name="name" 
+                    placeholder="Your Name" 
+                    required
+                /> <br/>
 
-                {/* <label for="email">Email:</label>  */}
-                <input type="email" name="email" placeholder="Your Email" required/> <br/>
+                <input 
+                    value=""
+                    type="email" 
+                    onChange={handleInputChange}
+                    name="email" 
+                    placeholder="Your Email" 
+                    required
+                /> <br/>
 
-                <div >
-                    {/* <label for="message">Message:</label>  */}
-                    <input type="text" name="message" placeholder="Message" id="textarea" required/> <br/>
-                </div>
-                <button type="submit">Send</button>
+                <input 
+                    value=""
+                    type="text" 
+                    onChange={handleInputChange}
+                    name="message" 
+                    placeholder="Message" 
+                    id="textarea" 
+                    required
+                /> <br/>
+
+                <button 
+                    type="submit"
+                    onClick={handleFormSubmit}>
+                      Send
+                </button>
             </form>
        </div>
     )
