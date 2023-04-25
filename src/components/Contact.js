c201fe57545f5fcba692b59877100e822f3d2a35
Theophilus <93491747+nwherman1724@users.noreply.github.com>
2023-04-25 06:42:35 -0400

    contact card update

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -6,12 +6,39 @@ function Contact() {
     const [userName, setUserName] = useState('');
     const [userEmail, setUserEmail] = useState('');
     const [userMessage, setUserMessage] = useState('');
+    const [nameShowRequired, setNameShowRequired] = useState(false);
+    const [emailShowRequired, setEmailShowRequired] = useState(false);
+    const [messageShowRequired, setMessageShowRequired] = useState(false);
 
     const handleInputChange = (e) => {
         const { name, value } = e.target;
     
-        return name = setUserName(value)
+        if(name === "name") {
+            setUserName(value)
+        }if (name === "email") {
+            setUserEmail(value)
+        }if (name === "message"){
+            setUserMessage(value)
+        }
       };
+
+      const handleBlur = (e) => {
+        if(!e.target.value && e.target.name === "name"){
+           setNameShowRequired(true);//display field required
+        }else if (!e.target.value && e.target.name === "email"){
+            setEmailShowRequired(true);
+        }else if (!e.target.value && e.target.name === "message"){
+            setMessageShowRequired(true);
+        }
+        else {
+            setNameShowRequired(false);
+            setEmailShowRequired(false);
+            setMessageShowRequired(false);
+        }
+      }
+
+    //   function email validation use regex
+    // https://regexr.com/2ri2c
 
     const handleFormSubmit = (e) => {
         e.preventDefault();
@@ -25,32 +52,41 @@ function Contact() {
                 <h3 class="contactTitle">Contact Me</h3>
 
                 <input 
-                    value=""
+                    value={userName}
                     type="text"
                     onChange={handleInputChange}
+                    onBlur= {handleBlur}
                     name="name" 
                     placeholder="Your Name" 
                     required
                 /> <br/>
 
+                {nameShowRequired && (<div>Field Required</div>)}
+
                 <input 
-                    value=""
+                    value={userEmail}
                     type="email" 
                     onChange={handleInputChange}
+                    onBlur= {handleBlur}
                     name="email" 
                     placeholder="Your Email" 
                     required
                 /> <br/>
 
+                {emailShowRequired && (<div>Field Required</div>)}
+
                 <input 
-                    value=""
+                    value={userMessage}
                     type="text" 
                     onChange={handleInputChange}
+                    onBlur= {handleBlur}
                     name="message" 
                     placeholder="Message" 
                     id="textarea" 
                     required
                 /> <br/>
+
+                {messageShowRequired && (<div>Field Required</div>)}
 
                 <button 
                     type="submit"
@@ -58,6 +94,7 @@ function Contact() {
                       Send
                 </button>
             </form>
+            
        </div>
     )
 }
