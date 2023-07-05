3045d6dc8f7ae58b9d7c3a55f198f5e13e5bd6a2
Theophilus <93491747+nwherman1724@users.noreply.github.com>
2023-07-05 11:09:39 -0400

    updated resume and nav

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,6 +1,6 @@
 import React from "react";
 import './Nav.css';
-import Resume from "../img/N.Herman Resume.pdf"
+import Resume from "../img/N.HermanResume.pdf"
 
 function Nav() {
 
@@ -8,10 +8,10 @@ function Nav() {
         <nav class="navbar">
           <div id="logo"><a href="#">Nick Herman</a></div>
           <ul>
-            <li class="middle" selected><a href="#About">About Me</a></li>
+            {/* <li class="middle" selected><a href="#About">About Me</a></li> */}
             <li class="middle"><a href="#work">Work</a></li>
             {/* <!-- <li class="middle"><a href="#Contact">Contact Me</a></li> --> */}
-            <li class="middle"><a href={ Resume } download="N.Herman Resume">Resume</a></li>
+            <li class="middle"><a href={ Resume } download="N.HermanResume.pdf">Resume</a></li>
           </ul>
           <a href="#Contact"><button class="right button">Contact Me</button></a>
         </nav>
